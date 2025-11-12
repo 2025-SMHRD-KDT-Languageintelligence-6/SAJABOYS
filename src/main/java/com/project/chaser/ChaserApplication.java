3cc4aa449da0704f82ed428fdfe08cc952f362fb
bsb0107 <bsb0107@gmail.com>
2025-11-12 15:49:18 +0900

    처음

---
+++ src/main/java/com/project/chaser/ChaserApplication.java
@@ -0,0 +1,13 @@
+package com.project.chaser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/chaser/ChaserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.chaser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
