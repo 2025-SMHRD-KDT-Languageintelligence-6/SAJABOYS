5ff64c31c6ac10ba7cea4f7521aa2c1c68eec6af
bsb0107 <bsb0107@gmail.com>
2025-11-13 10:32:12 +0900

    11/13 10:32

--- src/main/java/com/project/chaser/controller/FestivalController.java
+++ src/main/java/com/project/chaser/controller/FestivalController.java
@@ -1,10 +1,14 @@
 package com.project.chaser.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FestivalController {
     @GetMapping("/main")
-    return "index";
+    public String goMain() {
+        return "index";
+    }
 }
