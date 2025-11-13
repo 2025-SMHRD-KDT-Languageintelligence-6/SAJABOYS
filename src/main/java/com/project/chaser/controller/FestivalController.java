eaa9eac6f0c5ba1ec07d6053d36d6e9c492a0ec2
bsb0107 <bsb0107@gmail.com>
2025-11-13 10:37:31 +0900

    11/13 10:37

--- src/main/java/com/project/chaser/controller/FestivalController.java
+++ src/main/java/com/project/chaser/controller/FestivalController.java
@@ -1,14 +1,9 @@
 package com.project.chaser.controller;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FestivalController {
-    @GetMapping("/main")
-    public String goMain() {
-        return "index";
-    }
+
 }
--- src/main/java/com/project/chaser/controller/UserController.java
+++ src/main/java/com/project/chaser/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.project.chaser.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+    @GetMapping("/main")
+    public String goMain() {
+        return "index";
+    }
 }
